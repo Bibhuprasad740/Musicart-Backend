ba91b4c5f134cf66bb4ec528e2266cb21dbbd2a1
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2024-03-19 13:52:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT);
