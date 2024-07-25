c96402a2f250d3a47a8b81ef1a5ef03198c1491b
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2024-07-25 21:59:09 +0530

    Revert "fix: fixed request filter query"
    This reverts commit 9ef933c45f1805dd7b65aee3bf3cf0487b25e3ba.

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,49 +2,46 @@ const Product = require("../database/models/Product");
 
 exports.getAllProducts = async (req, res) => {
   try {
-    const filters = req.query.filters;
+    const filters = req.query.filters || {};
     const searchQuery = req.query.searchQuery || "";
 
     let query = Product.find();
     if (searchQuery !== "") {
       query = query.where("name").regex(new RegExp(searchQuery, "i"));
     }
-
-    if (filters) {
-      if (filters.typeFilter != "") {
-        query = query.where("type").equals(filters.typeFilter);
-      }
-      if (filters.companyFilter != "") {
-        query = query.where("brand").equals(filters.companyFilter.toUpperCase());
-      }
-      if (filters.colorFilter != "") {
-        const color =
-          filters.colorFilter.charAt(0).toUpperCase() +
-          filters.colorFilter.slice(1);
-        query = query.where("color").equals(color);
-      }
-      if (filters.priceFilter !== "") {
-        const minPrice = parseInt(filters.priceFilter.split("-")[0]);
-        const maxPrice = parseInt(filters.priceFilter.split("-")[1]);
-        query = query.where("price").gte(minPrice).lte(maxPrice);
-      }
-      if (filters.sortingFilter !== "") {
-        switch (filters.sortingFilter) {
-          case "low-high":
-            query = query.sort({ price: 1 });
-            break;
-          case "high-low":
-            query = query.sort({ price: -1 });
-            break;
-          case "a-z":
-            query = query.sort({ name: 1 });
-            break;
-          case "z-a":
-            query = query.sort({ name: -1 });
-            break;
-          default:
-            query = query.sort({ price: 1 });
-        }
+    if (filters.typeFilter != "") {
+      query = query.where("type").equals(filters.typeFilter);
+    }
+    if (filters.companyFilter != "") {
+      query = query.where("brand").equals(filters.companyFilter.toUpperCase());
+    }
+    if (filters.colorFilter != "") {
+      const color =
+        filters.colorFilter.charAt(0).toUpperCase() +
+        filters.colorFilter.slice(1);
+      query = query.where("color").equals(color);
+    }
+    if (filters.priceFilter !== "") {
+      const minPrice = parseInt(filters.priceFilter.split("-")[0]);
+      const maxPrice = parseInt(filters.priceFilter.split("-")[1]);
+      query = query.where("price").gte(minPrice).lte(maxPrice);
+    }
+    if (filters.sortingFilter !== "") {
+      switch (filters.sortingFilter) {
+        case "low-high":
+          query = query.sort({ price: 1 });
+          break;
+        case "high-low":
+          query = query.sort({ price: -1 });
+          break;
+        case "a-z":
+          query = query.sort({ name: 1 });
+          break;
+        case "z-a":
+          query = query.sort({ name: -1 });
+          break;
+        default:
+          query = query.sort({ price: 1 });
       }
     }
 
